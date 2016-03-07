664d5994bda83a2032d7bd8f1d26faa82aa26955
shaunbevan <tbevan@gmail.com>
2016-03-07 08:10:52 -0500

    Clean up

--- app.js
+++ app.js
@@ -6,32 +6,3 @@ var bundles = new Firebase('https://stardew.firebaseio.com/')
   }, function (err) {
     console.log("Error");
   });
-
-// bundles.on("value", function(snapshot) {
-//   var data = snapshot.val();
-//   var list = [];
-//   for (var key in data){
-//     if (data.hasOwnProperty(key)){
-//       name = data[key];
-//       console.log(name);
-//     }
-//     list.push(name);
-//   }
-//   var lis = '';
-//   for (var i = 0; i < list.length; i++) {
-//       lis += '<li data-key="' + list[i] + '">' + list[i] + '</li>';
-//   };
-//   console.log("list: " + lis);
-//   document.getElementById('items').innerHTML = lis;
-//
-// }, function (errorObject) {
-//   console.log("The read failed: " + errorObject.code);
-// });
-//
-// function refreshUI(list) {
-//     var lis = '';
-//     for (var i = 0; i < list.length; i++) {
-//         lis += '<li data-key="' + list[i].key + '">' + list[i].name + '</li>';
-//     };
-//     document.getElementById('items').innerHTML = lis;
-// };
