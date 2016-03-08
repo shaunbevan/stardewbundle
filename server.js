94d773bfee6f4b8e40144d9c7a7c03d63abbc9dd
shaunbevan <tbevan@gmail.com>
2016-03-07 20:42:36 -0500

    Add node dependencies

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
 });
+=======
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
+>>>>>>> Stashed changes
